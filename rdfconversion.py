2abd39c390a1c51d2ff9faf8634822081047ad58
valentinabertelli <valentina.bertelli2@studio.unibo.it>
2024-05-07 21:52:14 +0200

    Update rdfconversion.py

--- rdfconversion.py
+++ rdfconversion.py
@@ -34,33 +34,29 @@ df_monument = pd.read_csv("csv files/lighthouse_of_alexandria.csv")
 for _, row in df_monument.iterrows():
 
     subject_uri = URIRef(item + row["Subject"])
-    predicate = row["Predicate"]
+    predicate = URIRef(row["Predicate"])
     obj = row["Object"]
+    
+    if predicate in [RDF.type, OWL.sameAs]:
+        obj = URIRef(obj)
+    elif predicate in [DC.creator, CDWA.Commissioner, SCHEMA.agent]:
+        obj = URIRef(person + obj).replace(" ", "_")
+    elif predicate in [DCTERMS.spatial, CDWA.CurrentLocation]:    
+        obj = URIRef(place + obj).replace(" ", "_")
+    elif predicate is DCTERMS.isPartOf:
+        obj = URIRef(group + obj).replace(" ", "_")
+    else:
+        obj = Literal((obj), datatype=XSD.string)    
 
+    g.add((subject_uri, predicate, obj))    
+
+g.serialize("output3.ttl", format="turtle", encoding="utf8")
+
+'''
     if "rdf" in predicate:
         ps = predicate.split(":")
         predicate_uri = RDF + ps[1]
         obj = URIRef(obj)
     
-    g.add((subject_uri, predicate_uri, obj))  
-
-g.serialize("output3.ttl", format="turtle", encoding="utf8")
-
-'''
-obj = row["Object"]
-        
-if predicate in [RDF.type, OWL.sameAs]:
-    obj = URIRef(obj)
-elif predicate in [DC.creator, CDWA.Commissioner, SCHEMA.agent]:
-    obj = URIRef(person + obj).replace(" ", "_")
-elif predicate in [DCTERMS.spatial, CDWA.CurrentLocation]:    
-    obj = URIRef(place + obj).replace(" ", "_")
-elif predicate is DCTERMS.isPartOf:
-    obj = URIRef(group + obj).replace(" ", "_")
-else:
-    obj = Literal((obj), datatype=XSD.string)    
-
-g.add((subject_uri, predicate, obj))    
-
-g.serialize("output3.ttl", format="turtle", encoding="utf8")
-'''
+        g.add((subject_uri, predicate_uri, obj))  
+'''        
