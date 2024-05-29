edd2d67577920f1666a7a70e59cdcfb981ddefd8
elena2notti <146434685+elena2notti@users.noreply.github.com>
2024-05-29 20:18:32 +0200

    paura

--- assets/js/main.js
+++ assets/js/main.js
@@ -471,39 +471,26 @@ function myFunction(inp) {
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.mobile-nav-toggle');
+    const navbar = document.querySelector('#navbar');
+    const navbarUl = navbar.querySelector('ul');
 
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+    toggleButton.addEventListener('click', function () {
+      navbar.classList.toggle('navbar-mobile');
+      toggleButton.classList.toggle('bi-x');
+    });
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
+    const dropdownLinks = navbar.querySelectorAll('.dropdown > a');
+    dropdownLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+        if (navbar.classList.contains('navbar-mobile')) {
+          e.preventDefault();
+          this.nextElementSibling.classList.toggle('dropdown-active');
+        }
+      });
+    });
+  });
 
   /**
    * Scroll with ofset on page load with hash links in the url
