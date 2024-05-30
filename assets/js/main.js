1d3324b26f085683f7dc02244412ad1af2160df2
elena2notti <146434685+elena2notti@users.noreply.github.com>
2024-05-30 12:37:43 +0200

    better tei & study of domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -471,26 +471,39 @@ function myFunction(inp) {
   /**
    * Mobile nav toggle
    */
-  document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.mobile-nav-toggle');
-    const navbar = document.querySelector('#navbar');
-    const navbarUl = navbar.querySelector('ul');
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
 
-    toggleButton.addEventListener('click', function () {
-      navbar.classList.toggle('navbar-mobile');
-      toggleButton.classList.toggle('bi-x');
-    });
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
 
-    const dropdownLinks = navbar.querySelectorAll('.dropdown > a');
-    dropdownLinks.forEach(link => {
-      link.addEventListener('click', function (e) {
-        if (navbar.classList.contains('navbar-mobile')) {
-          e.preventDefault();
-          this.nextElementSibling.classList.toggle('dropdown-active');
-        }
-      });
-    });
-  });
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
 
   /**
    * Scroll with ofset on page load with hash links in the url
