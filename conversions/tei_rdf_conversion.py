fe8e322d626d5911a04c379fa80b4fd71f38902d
valentinabertelli <valentina.bertelli2@studio.unibo.it>
2024-06-02 23:42:40 +0200

    finished tei rdf convsion

--- conversions/tei_rdf_conversion.py
+++ conversions/tei_rdf_conversion.py
@@ -38,7 +38,7 @@ when = date.get("when")
 
 #metadata uris
 title_uri = URIRef(pt + title.replace(" ", "_"))
-author_uri = URIRef(pt + author.replace(" ", "_"))
+author_uri = URIRef(pt + author.replace(",", "").replace(" (1934-2019)", "").replace(" ", "_"))
 publisher_uri = URIRef(pt + publisher.replace(" ", "_"))
 
 #adding metadata triples to the graph
@@ -78,17 +78,17 @@ for character in tree.findall(".//tei:profileDesc/tei:particDesc/tei:listPerson/
 lines_dict = dict()
 
 #counter
-counter = 1
+counter_l = 1
 
 #find lines
 for line in tree.findall(".//tei:text/tei:body/tei:div/tei:div/tei:l", ns):
 
     #create id and uri for every line
-    line_id = counter
+    line_id = counter_l
     line_uri = URIRef(pt + "line/" + str(line_id))
     #add id and uri to dict
     lines_dict[line_id] = line_uri 
-    counter += 1
+    counter_l += 1
 
     #find text of line
     text = ''.join(line.itertext()) 
@@ -96,13 +96,15 @@ for line in tree.findall(".//tei:text/tei:body/tei:div/tei:div/tei:l", ns):
     #find if characters are mentioned
     mention = line.find("tei:name[@type='person']", ns)
     if mention is not None:
-        ref = mention.get("ref").strip('#')
-        if ref in character_dict:
-            character_uri = character_dict[ref]
-            g.add((line_uri, RDF.type, SCHEMA.Text))
-            g.add((line_uri, RDFS.label, Literal(text))) 
-            g.add((line_uri, DCTERMS.references, character_uri))
-            g.add((character_uri, DCTERMS.isReferencedBy, line_uri))
+        ref = mention.get("ref")
+        if ref is not None:
+            ref = ref.strip('#')
+            if ref in character_dict:
+                character_uri = character_dict[ref]
+                g.add((line_uri, RDF.type, SCHEMA.Text))
+                g.add((line_uri, RDFS.label, Literal(text))) 
+                g.add((line_uri, DCTERMS.references, character_uri))
+                g.add((character_uri, DCTERMS.isReferencedBy, line_uri))
 
     #find if places are mentioned
     place = line.find("tei:name[@type='place']", ns)
@@ -115,14 +117,61 @@ for line in tree.findall(".//tei:text/tei:body/tei:div/tei:div/tei:l", ns):
         g.add((line_uri, RDFS.label, Literal(text)))
         g.add((line_uri, DCTERMS.references, place_uri))        
 
-#find quotations
-for q in tree.findall(".//tei:text/tei:body/tei:div/tei:div/tei:l/tei:q", ns): 
-    quotation = ''.join(q.itertext())
-    quotation_uri = URIRef(pt + "quotation/" + quotation.replace(" ", "_"))
-    speaker = q.get("who").strip('#')
-    speaker_uri = character_dict[speaker]
-    g.add((quotation_uri, RDF.type, SCHEMA.Quotation))  
-    g.add((quotation_uri, SCHEMA.spokenByCharacter, speaker_uri))              
+#function to unite text of quotes that are in separate lines
+def unite_quotes_text(link, root):
+    targets = link.get("target").split()
+    quotes = []
+    for qs in targets:
+        qs = qs.strip('#')
+        xml_ns = "{http://www.w3.org/XML/1998/namespace}"
+        s = root.find(f".//tei:s[@{xml_ns}id='{qs}']", ns)
+        if s is not None:
+            quotes.append(''.join(s.itertext()))
+    return ' '.join(quotes)
+
+#dictionary for links
+link_dict = dict()
+
+#find text of quotes that must be linked to each other
+for link in tree.findall(".//tei:text/tei:body/tei:div/tei:div/tei:link", ns):
+    complte_quote_text = unite_quotes_text(link, tree.getroot())
+    target = link.get("target").split()
+    complete_quote_id = target[0].strip('#')
+    link_dict[complete_quote_id] = complte_quote_text
+
+#dictionary for quotes and their text
+quote_text_dict = dict()
+
+#find all the quotes
+for quote in tree.findall(".//tei:text/tei:body/tei:div/tei:div/tei:l/tei:q", ns):
+
+    #find quote text
+    s = quote.find("tei:s", ns)
+    if s is not None:
+        id = s.get("{http://www.w3.org/XML/1998/namespace}id")
+        if id in link_dict:
+            quote_text = link_dict[id] 
+            quote_text_dict[quote] = quote_text 
+    else:        
+        quote_text = ''.join(quote.itertext())
+        quote_text_dict[quote] = quote_text 
+
+    #counter
+    counter_q = 1   
+    
+    for q, text in quote_text_dict.items():
+
+        #create id and uri for every quote
+        quote_id = counter_q
+        counter_q += 1
+        quote_uri = URIRef(pt + "quote/" + str(quote_id))
+    
+        #find speaker
+        speaker = q.get("who").strip('#')
+        speaker_uri = character_dict[speaker]
+        g.add((quote_uri, RDF.type, SCHEMA.Quotation))
+        g.add((quote_uri, RDFS.label, Literal(text))) 
+        g.add((quote_uri, SCHEMA.spokenByCharacter, speaker_uri))
 
 #serialize the graph to Turtle format
 turtle_str = g.serialize(format="turtle", base=pt, encoding="utf-8")
@@ -130,4 +179,3 @@ turtle_str = g.serialize(format="turtle", base=pt, encoding="utf-8")
 #write the Turtle string to a file
 with open("output_tei_rdf.ttl", "wb") as f:
     f.write(turtle_str)
-
